a29eedfda5bdb5c963e12be25960d2f9047c2613
prapti-r <prapti.risal12@gmail.com>
2025-01-04 18:25:22 +0545

    javascript added

---
+++ c7466870/mysite/static/js/script.js
@@ -0,0 +1,6 @@
+// Search Button Toggle Functionality
+
+document.querySelector('#burger').addEventListener('click', () => {
+    console.log('Burger icon clicked'); // Debug message
+    document.querySelector('#nav-links').classList.toggle('is-active');
+});
