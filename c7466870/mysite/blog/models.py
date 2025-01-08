f8b8b344bc478c24e6a837c8a152ab61f993edf9
prapti-r <prapti.risal12@gmail.com>
2025-01-08 17:03:04 +0545

    models added

--- c7466870/mysite/blog/models.py
+++ c7466870/mysite/blog/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100, unique=True)
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField("Category", related_name="posts")
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    summary = models.TextField()
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
