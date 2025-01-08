f45459d1aa166550f23a4a43845dd7a0d9284dcd
prapti-r <prapti.risal12@gmail.com>
2025-01-08 21:53:58 +0545

    blog url modified

--- c7466870/mysite/mysite/urls.py
+++ c7466870/mysite/mysite/urls.py
@@ -15,15 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import  views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('services/', views.services, name='services'),
-    path('blog/', views.blog, name='blog'),
     path('contact/', views.contact, name='contact'),
-
+    path('blog/', include('blog.urls')),
+    path('blogdetail/<int:id>', views.blogdetail, name="blogdetail"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
