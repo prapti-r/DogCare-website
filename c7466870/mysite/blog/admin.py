33fa2ed115e5f59458d6f410a62e971a7482ea2b
prapti-r <prapti.risal12@gmail.com>
2025-01-08 17:04:33 +0545

    admin added

--- c7466870/mysite/blog/admin.py
+++ c7466870/mysite/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlogPost, Category
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at')
+    search_fields = ('title', 'author')
+    list_filter = ('category', 'created_at')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
