d78f5e3e583c4b95995fe86e5b0c0ad23ea9a119
prapti-r <prapti.risal12@gmail.com>
2025-01-10 19:25:33 +0545

    slug added to blog models

--- c7466870/mysite/blog/models.py
+++ c7466870/mysite/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template.defaultfilters import slugify  
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -8,12 +10,22 @@ class Category(models.Model):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='media/blog_images/', null=True, blank=True)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     summary = models.TextField()
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    slug = models.SlugField(null=True, unique=True)
+    
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):  
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
