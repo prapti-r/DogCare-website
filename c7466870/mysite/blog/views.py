8f2cdd4c6f114383e624d939d8c892a14dbd467f
prapti-r <prapti.risal12@gmail.com>
2025-01-13 08:02:19 +0545

    blogdetail view updated

--- c7466870/mysite/blog/views.py
+++ c7466870/mysite/blog/views.py
@@ -9,6 +9,7 @@ def blog_list(request):
 
 def blog_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug)
-    return render(request, 'blog/blog_detail.html', {'post': post})
+    categories = Category.objects.all()
+    return render(request, 'blog/blog_detail.html', {'post': post, 'categories': categories})
 
 
