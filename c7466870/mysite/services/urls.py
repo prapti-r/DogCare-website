e24509e7ab7beae2e908888d5cb0e691d058a965
prapti-r <prapti.risal12@gmail.com>
2025-01-13 20:54:37 +0545

    services urls added

---
+++ c7466870/mysite/services/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  
+
+urlpatterns = [
+    path('', views.services_list, name='services_list'),
+    path('<slug:slug>/', views.services_detail, name='services_detail'),
+]
