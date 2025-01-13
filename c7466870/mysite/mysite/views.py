7e567be15dc5e74416b91a76122e6fa03043d14a
prapti-r <prapti.risal12@gmail.com>
2025-01-13 20:53:51 +0545

    services views added

--- c7466870/mysite/mysite/views.py
+++ c7466870/mysite/mysite/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from blog.models import BlogPost, Category
+from services.models import ServiceCategory
+from services.forms import BookingForm
 
 def home(request):
     blogs_posts = BlogPost.objects.all().order_by('-created_at')[:3]  
@@ -9,9 +11,25 @@ def home(request):
 def aboutus(request):
     return render(request, "aboutus.html")
 
-def services(request):
-    return render(request, "services.html")
 
+def services(request):
+    categories = ServiceCategory.objects.all()
+    return render(request, 'services/services_list.html', {'categories': categories})
+
+def servicedetail(request, category_slug):
+    category = get_object_or_404(ServiceCategory, slug=category_slug)
+    form = BookingForm()
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save()
+            # Optional: Add a success message or redirect
+            return redirect('services_list')
+
+    return render(request, 'services/service_detail.html', {'category': category, 'form': form})
+
+    
 def contact(request):
     return render(request, "contact.html")
 
