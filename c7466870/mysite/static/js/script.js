0c81a2f1e8e5dcd84e1365f4ddbfcb0d731f81d6
prapti-r <prapti.risal12@gmail.com>
2025-01-14 00:05:20 +0545

    files updated

--- c7466870/mysite/static/js/script.js
+++ c7466870/mysite/static/js/script.js
@@ -4,3 +4,5 @@ document.querySelector('#burger').addEventListener('click', () => {
     console.log('Burger icon clicked'); // Debug message
     document.querySelector('#nav-links').classList.toggle('is-active');
 });
+
+
